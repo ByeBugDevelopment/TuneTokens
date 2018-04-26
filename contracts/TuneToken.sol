384ac7ac30d69d23deea4d3eb8be68eb71823630
Christopher Arguello <christopheraarguello@gmail.com>
2018-04-26 15:56:10 -0400

    corrected 2 critical errors from HOSHO audit

--- contracts/TuneToken.sol
+++ contracts/TuneToken.sol
@@ -14,8 +14,8 @@ contract TuneToken is Pausable, StandardToken {
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
-  function SimpleToken() public {
-    totalSupply_ = INITIAL_SUPPLY;
+  function TuneToken() public {
+    _totalSupply = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
     Transfer(0x0, msg.sender, INITIAL_SUPPLY);
   }
